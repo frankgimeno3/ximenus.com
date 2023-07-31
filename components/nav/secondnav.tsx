f197cd66813fbf73aa479cbadffdcc4abefc7fe7
Frank Gimeno <frankgimeno3@gmail.com>
2023-07-31 13:51:27 +0200

    creo contenido flexible json

--- components/nav/secondnav.tsx
+++ components/nav/secondnav.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Navtop: React.FC = () => {
+const Secondnav: React.FC = () => {
   return (
     <nav className='flex flex-row bg-white bg-opacity-20 p-5 justify-between'>
+      <h2 className='text-4xl font-bold'>Softwar3.com</h2>
       <div className='flex flex-row align-center pt-2'>
-      <button className='bg-cyan-900 hover:bg-cyan-800 mx-2 rounded-lg text-white px-3 py-1 text-md border border-gray-500 shadow'>Contacto</button>
+      <button className='bg-cyan-900 hover:bg-cyan-800 mx-2 rounded-lg text-white px-3 py-1 text-md border border-gray-500 shadow'>Section 1</button>
       <button className='bg-cyan-900 hover:bg-cyan-800 mx-2 rounded-lg text-white px-3 py-1 text-md border border-gray-500 shadow'>Section 2</button>
       <button className='bg-cyan-900 hover:bg-cyan-800 mx-2 rounded-lg text-white px-3 py-1 text-md border border-gray-500 shadow'>Section 3</button>
       <button className='bg-cyan-900 hover:bg-cyan-800 mx-2 rounded-lg text-white px-3 py-1 text-md border border-gray-500 shadow'>Section 4</button>
@@ -14,4 +15,4 @@ const Navtop: React.FC = () => {
   );
 };
 
-export default Navtop;
+export default Secondnav;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,51 @@
+import { useState, useEffect } from "react";
 import Image from "next/image";
-import { Inter } from "next/font/google";
-import Seccion1 from "../components/content/seccion1/seccion1";
-import Seccion2 from "../components/content/seccion2/seccion2";
-import Seccion3 from "../components/content/seccion3/seccion3";
-import Seccion4 from "../components/content/seccion4/seccion4";
-import Seccion5 from "../components/content/seccion5/seccion5";
+import Seccion1 from "../components/content/1videolong/seccion1";
+import Seccion2 from "../components/content/2bannerlong/seccion2";
+import Seccion3 from "../components/content/3cuadricula/seccion3";
+import Seccion4 from "../components/content/4rowcontent/seccion4";
+import Seccion5 from "../components/content/5footer/seccion5";
+import Navtop from "../components/nav/navtop";
+import Navsecond from "../components/nav/secondnav";
+import langes from "../lib/es.json";
+import langen from "../lib/en.json";
 
-const inter = Inter({ subsets: ["latin"] });
+ interface LanguageObject {
+   [key: string]: string;
+}
 
 export default function Home() {
+  const [lang, setLang] = useState("es");
+  const [langprop, setLangProp] = useState<LanguageObject | undefined>(undefined);
+
+  useEffect(() => {
+     const setLanguageProperty = () => {
+      switch (lang) {
+        case "es":
+          setLangProp(langes);
+          break;
+        case "en":
+          setLangProp(langen);
+          break;
+        default:
+           setLangProp(langes);
+          break;
+      }
+    };
+
+     setLanguageProperty();
+  }, [lang]);  
+  
   return (
-    <div className="min-h-screen  ">
-      <div className="bg-gradient-to-t from-cyan-700 to-cyan-900 w-full h-full text-white  w-full">
+    <div className="min-h-screen">
+      <Navtop />
+      <Navsecond />
+      <div className="bg-gradient-to-t from-cyan-700 to-cyan-900 w-full h-full text-white w-full">
         <Seccion1 />
         <div className="px-24">
-        <Seccion2 />
-        <Seccion3 />
-        <Seccion4 />
+          <Seccion2 />
+          <Seccion3 />
+          <Seccion4 />
         </div>
         <Seccion5 />
       </div>
